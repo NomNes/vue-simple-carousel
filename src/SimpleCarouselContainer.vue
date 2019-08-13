4ab03ddd7cf92d71ce574ac966fb9210192136f3
nomnes <nomnes1990@gmail.com>
2019-08-14 05:43:58 +0700

    hide controls if list.length < 2

--- src/SimpleCarouselContainer.vue
+++ src/SimpleCarouselContainer.vue
@@ -3,7 +3,7 @@
         .vsc-scroller(@touchstart="touchStart")
             .vsc-items(:style="{width: `${itemsLength * 100}%`}" ref="items" :class="{moving}")
                 slot
-        .vsc-controls
+        .vsc-controls(v-if="itemsLength < 2")
             .arrow.prev(v-if="hasPrev" @click="setCurrentIndex(currentIndex - 1)") &larr;
             .vsc-points
                 .vsc-point(v-for="point in points" :key="point" :class="{current: currentIndex === point}" @click="setCurrentIndex(point)")
